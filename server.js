8c5e80f771e6bdbb780698dfc7663c9b9067aef0
akahomosapien <thisisankityadav@gmail.com>
2026-09-15 21:51:11 +0530

    chore:add database configfor mongodb

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./src/config/db.config.js";
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = () => {
+const startServer = async () => {
   try {
+    await connectDB();
     app.listen(PORT, () => {
       console.log(`Server started at PORT:${PORT}`);
     });
