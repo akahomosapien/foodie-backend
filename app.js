e910ad39de87875715ce2367f8a00c9d7f2ea8e8
akahomosapien <thisisankityadav@gmail.com>
2026-09-15 22:10:20 +0530

    chore:add middleware and utilities config

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import errorMiddleware from "./src/middlewares/error.middleware";
 
 const app = express();
+
+//Middlewares
+
+app.use(errorMiddleware); //keep this at last
 
 export default app;
